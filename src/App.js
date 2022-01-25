cadd383ff48876bb1e155eece1fac4a252adf168
sherica123 <71396059+sherica123@users.noreply.github.com>
2022-01-25 18:21:39 +0400

    latest-settings

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ import Horror from './pages/CatPages/Horror'
 import Humour from './pages/CatPages/Humour.js'
 import NonFiction from './pages/CatPages/NonFiction'
 import Romance from './pages/CatPages/Romance'
+import About from "./pages/About/about";
+import Account from "./pages/Account/account";
+import Security from "./pages/Security/security";
+import Logout from "./pages/Logout/logout";
 
+import Settings from "./pages/Settings/Settings";
 // import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
@@ -27,6 +32,15 @@ function App() {
 				<Route path='/category/humour' element={<Humour />} />
 				<Route path='/category/nonFiction' element={<NonFiction />} />
 				<Route path='/category/romance' element={<Romance />} />
+				<Route path="/settings" exact element={<Settings/>} /> 
+
+				<Route path="/about" exact element={<About/>} />
+				
+				<Route path="/account" exact element={<Account/>} />
+				
+				<Route path="/security" exact element={<Security/>} />
+				
+				<Route path="/logout" exact element={<Logout/>} />
 			</Routes>
 		</div>
 	)
